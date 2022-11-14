90bfdfb0efd9b891c957ff3dff81388fc9d3d3cb
Jack Venberg <jack.venberg@rover.com>
2022-11-13 23:34:51 -0800

    Fix install and import

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#!/bin/bash
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                 cloning powerlevel10k                       "
+    echo "-----------------------------------------------------------"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "==========================================================="
+    echo "                  Import p10k.zsh                             "
+    echo "-----------------------------------------------------------"
+    cp .p10k.zsh $HOME/.p10k.zsh
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cp .zshrc $HOME/.zshrc
+}
+
+zshrc
